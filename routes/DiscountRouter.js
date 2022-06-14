1b523318d4a31cf703599dbfd7f773eb94704bd8
chuanluu0103 <chuanluu0103@gmail.com>
2022-06-14 10:56:15 +0700

    14/06_D-d

--- routes/DiscountRouter.js
+++ routes/DiscountRouter.js
@@ -1,13 +1,12 @@
-const router = require('express').Router()
-const discountCtrl = require('../controllers/discountCtrl')
-const auth = require('../middleware/authen')
-const authAdmin = require('../middleware/authenAdmin')
+const router = require("express").Router();
+const discountCtrl = require("../controllers/discountCtrl");
+const auth = require("../middleware/authen");
 
 router
-  .route('/discount')
+  .route("/discount")
   .get(auth, discountCtrl.getVouchers)
-  .post(auth, discountCtrl.createVoucher)
+  .post(auth, discountCtrl.createVoucher);
 
-router.patch('/discount/:id', auth, discountCtrl.updateVoucher)
-router.post('/discount/checkvalid', discountCtrl.checkValidDiscount)
-module.exports = router
+router.patch("/discount/:id", auth, discountCtrl.updateVoucher);
+router.post("/discount/checkvalid", discountCtrl.checkValidDiscount);
+module.exports = router;
